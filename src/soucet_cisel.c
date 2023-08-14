6105bdc1fceaa25b520e7ec57dfd8dc1ad73e743
ekral <ekral@utb.cz>
2023-08-14 12:44:28 +0200

    Create soucet_cisel.c

---
+++ src/soucet_cisel.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	// Nebudeme resit osetreni nespravneho vstupu kvuli zjednodusseni.
+
+	int x = 0;
+	int y = 0;
+
+	printf("Enter x value:");
+	scanf_s("%d", &x ); // adresa promenne x
+	
+	printf("\n");
+	
+	printf("Enter y value:");
+	scanf_s("%d", &y ); // adresa promenne y
+	
+	// Spocitat BMI
+	// budeme potrebovat operaci deleni s desetinnou carkou
+	int z = x + y;
+
+	printf("\n");
+	printf("Value of the variable z is %d", z);
+
+	return 0;
+}
